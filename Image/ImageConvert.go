2f725a7086eb1d39af750c6c5eb97235cd518bdc
mooncode163 <chyfemail163@163.com>
2021-06-06 08:43:14 +0800

    ui

--- Image/ImageConvert.go
+++ Image/ImageConvert.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	// "strings"
@@ -17,26 +18,42 @@ import (
 	"github.com/nfnt/resize"
 )
 
+// 参数 1 转换前图片 2 转换后图片 3 widht 4 height
 func main() {
 	fmt.Println("ConverImage")
 
 	filesrc := "1.jpg"
 	filedst := "dst.png"
+	strw := "512"
+	strh := "512"
 	if len(os.Args) > 1 {
 		// who = strings.Join(os.Args[1:], " ")
 		filesrc = os.Args[1]
 		filedst = os.Args[2]
+		strw = os.Args[3]
+		strh = os.Args[4]
 	}
 	fmt.Println("image=", filesrc)
-	ConverImage(filesrc, filedst, 512, 1024)
+	width := 1
+	height := 1
+
+	if w, err := strconv.ParseInt(strw, 10, 32); err == nil {
+		width = int(w)
+		fmt.Printf("%T, w=%v\n", w, w)
+	}
+	if h, err := strconv.ParseInt(strh, 10, 32); err == nil {
+		fmt.Printf("%T, h=%v\n", h, h)
+		height = int(h)
+	}
+	ConverImage(filesrc, filedst, width, height)
 	// ConverImage(filesrc, "dst.png", 512, 1024)
 }
 
 // imagetype 0 jpg  1 png
-func ConverImage(filesrc string, filedst string, width uint, height uint) {
+func ConverImage(filesrc string, filedst string, width int, height int) {
 	file1, _ := os.Open(filesrc) //打开图片1
 	defer file1.Close()
-
+	fmt.Printf("ConverImage, width=", width, " height=", height)
 	// image.Decode 图片
 	var (
 		img1 image.Image
@@ -47,7 +64,7 @@ func ConverImage(filesrc string, filedst string, width uint, height uint) {
 		return
 	}
 	// 调用resize库进行图片缩放(高度填0，resize.Resize函数中会自动计算缩放图片的宽高比)
-	m1 := resize.Resize(width, height, img1, resize.Lanczos3)
+	m1 := resize.Resize(uint(width), uint(height), img1, resize.Lanczos3)
 
 	// 将两个图片合成一张
 	newWidth := m1.Bounds().Max.X                                      //新宽度 = 随意一张图片的宽度
